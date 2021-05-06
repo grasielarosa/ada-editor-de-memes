afa98480a24e797a058f29b1dac74e66b377c06a
grasielarosa <grasielarosaa@gmail.com>
2021-05-06 17:18:44 +0200

    change background color function for image / input color initial style

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 // ---------------- UPLOAD DEL IMAGE PARA EL MEME ---------------- 
 const link = document.getElementById('url');
 const image = " "
-const insertImage = (event) =>{
+const insertImage = () => {
     const insertImage = document.getElementById('imageMeme');
     insertImage.style.backgroundImage = `url("${link.value}")`;
 }
 link.addEventListener('change', insertImage);
 // ---------------- FIN DEL UPLOAD DEL IMAGE PARA EL MEME ---------------- 
-
-
+const pickUpColor = document.getElementById('colorPickerImage');
+const changeColor = () => {
+    const colorMeme = document.getElementById('imageMeme')
+    colorMeme.style.backgroundColor = pickUpColor.value
+}
+pickUpColor.addEventListener('input', changeColor);
+// ---------------- CAMBIO DEL COLOR DE FONDO IMAGE PARA EL MEME ----------------
