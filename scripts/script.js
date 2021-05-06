9d9c03030f3bc085ec2ac7bce54db7a1bcc39d32
grasielarosa <grasielarosaa@gmail.com>
2021-05-06 14:57:18 +0200

    update upload image function

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,9 @@ const link = document.getElementById('url');
 const image = " "
 const insertImage = (event) =>{
     const insertImage = document.getElementById('imageMeme');
-    insertImage.innerHTML = `<img src="${link.value}" alt="imagen">`
+    insertImage.style.backgroundImage = `url("${link.value}")`;
 }
 link.addEventListener('change', insertImage);
-
 // ---------------- FIN DEL UPLOAD DEL IMAGE PARA EL MEME ---------------- 
+
+
