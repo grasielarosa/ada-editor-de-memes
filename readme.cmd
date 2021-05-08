320efd99c516e6a2d13113b90c4a736ab747084f
grasielarosa <grasielarosaa@gmail.com>
2021-05-08 12:57:17 +0200

    image filters javascript and html

---
+++ readme.cmd
@@ -0,0 +1,11 @@
+### Editor de Memes ###
+
+Proyecto desarollado como trabajo práctico del módulo 2 (javascript) del curso de frontend de Ada ITW.
+Desarooladoras: Grasiela da Rosa:
+                Mariana Delau:
+                Yanina Bucca:
+Observaciones para leer el código:
+- las clases están descritas con guiones;
+- las id están descritas con [no me acuerdo el nombre]
+
+Abrirlo con Live server.
--- scripts/script.js
+++ scripts/script.js
@@ -21,3 +21,34 @@ const changeBlend = () => {
     colorMeme.style.backgroundBlendMode = colorBlend.value;
 }
 colorBlend.addEventListener('input', changeBlend);
+
+//---------------- CAMBIO DE FILTRO IMAGE PARA EL MEME ----------------
+const filterBlend = document.getElementsByClassName('input-filter');
+const colorMeme = document.getElementById('imageMeme');
+const brightness=document.getElementById('brightnessSlider');
+const opacity=document.getElementById('opacitySlider');
+const contrast=document.getElementById('contrastSlider');
+const blur=document.getElementById('blurSlider');
+const grayscale=document.getElementById('grayScaleSlider');
+const sepia=document.getElementById('sepiaSlider');
+const hueRotation=document.getElementById('hueRotateSlider');
+const saturation=document.getElementById('saturateSlider');
+const invert=document.getElementById('invertSlider');
+
+const changeFilter = () => {
+    let brightnessValue= brightness.value;
+    let opacityValue= opacity.value;
+    let contrastValue= contrast.value;
+    let blurValue= blur.value;
+    let grayscaleValue= grayscale.value;
+    let sepiaValue= sepia.value;
+    let hueRotationValue= hueRotation.value;
+    let saturationValue= saturation.value;
+    let invertValue= invert.value;
+    colorMeme.style.filter = `brightness(${brightnessValue}) opacity(${opacityValue}) blur(${blurValue}px) contrast(${contrastValue}%) grayscale(${grayscaleValue}%) hue-rotate(${hueRotationValue}deg) sepia(${sepiaValue}%) saturate(${saturationValue}%) invert(${invertValue})`;
+}
+
+for(i=0; i<filterBlend.length; i++ ){
+    
+filterBlend[i].addEventListener('input', changeFilter);
+}
