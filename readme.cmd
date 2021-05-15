583922a69b0fd8e38b76fc5e3d8aedad30bbea30
grasielarosa <grasielarosaa@gmail.com>
2021-05-15 13:44:29 +0200

    integrate remote changes by readme

--- readme.cmd
+++ readme.cmd
@@ -1,3 +1,5 @@
+### English and Portuguese translation below.
+
 ### español
 # Editor de Memes
 ## Proyecto desarollado como trabajo práctico del módulo 2 (javascript) para la [carrera de Frontend](https://adaitw.org/carreras/) de [Ada ITW](https://adaitw.org/).
@@ -13,3 +15,38 @@
 
 
 ## Abrirlo con Live server.
+
+
+
+### English
+# Meme Generator
+## Project developed as an Javascript practice to [front-end career](https://adaitw.org/carreras/) at [Ada ITW](https://adaitw.org/). 
+
+### Developers:
+- Grasiela da Rosa: [portafólio](https://grasielarosa.github.io/ada-portfolio-project/)
+- Mariana Delau: [portafólio](https://ybucca.github.io/PortafolioYB/)
+- Yanina Bucca: [portafólio](https://marianadeleau.github.io/ada-8va-portafolio/)
+
+
+### How to read this code:
+-	Class name were written with hyphen-minus
+-	Id names were written with camelCase.
+
+## Better open this code with Live Server.
+
+
+### Português
+# Gerador de Memes
+## Projeto desenvolvido como trabalho prático de Javascript para a [formação de Frontend](https://adaitw.org/carreras/) da [Ada ITW](https://adaitw.org/). 
+
+### Desenvolvedoras:
+- Grasiela da Rosa: [portafólio](https://grasielarosa.github.io/ada-portfolio-project/)
+- Mariana Delau: [portafólio](https://ybucca.github.io/PortafolioYB/)
+- Yanina Bucca: [portafólio](https://marianadeleau.github.io/ada-8va-portafolio/)
+
+
+### Observações para ler esse código:
+-	As classes estão descritas com traços.
+-	Os Id estão descritos com camelCase.
+
+## Abrir com Live Server.
